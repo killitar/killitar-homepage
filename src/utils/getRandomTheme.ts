aa62f89ab95d0b3ec58af9aa705fb48c23af687d
Miroslav Lincovsci <lincovsci02@gmail.com>
2024-02-26 21:58:03 +0300

    feat(component): add theme switch button

--- src/utils/getRandomTheme.ts
+++ src/utils/getRandomTheme.ts
@@ -1,11 +1,11 @@
 interface Theme {
   id: number;
-  themeName: string;
+  name: string;
 }
 
 const getRandomTheme = (themesList: Theme[]): string => {
   const randomIndex: number = Math.floor(Math.random() * themesList.length);
-  const theme = themesList[randomIndex].themeName;
+  const theme = themesList[randomIndex].name;
   console.log(theme);
   return theme;
 };
