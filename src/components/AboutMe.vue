0d60307c960cb44ae6751c20eb7231546ae5719c
Miroslav Lincovsci <lincovsci02@gmail.com>
2024-03-03 19:14:24 +0300

    feat(components): add:components(AboutMe,SocialLinks)

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2 class="mb-10 text-4xl text-secondary duration-300">
+      hello,<br />
+      i'm <span class="text-primary">miroslav lincovsci</span>.👋
+    </h2>
+    <p class="text-lg text-secondary duration-300">
+      a front-end web developer, currently studying at the Russian University of
+      Cooperation. i love how the web can connect people, it inspires me to get
+      better and keep learning.
+    </p>
+  </div>
+</template>
