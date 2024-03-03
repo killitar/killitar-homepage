1bffb365a1e169eab4a587dde43e993458d1e50c
Miroslav Lincovsci <lincovsci02@gmail.com>
2024-03-03 19:43:45 +0300

    fix(component): rename ProjectsDropdown

---
+++ src/components/ProjectsAccordion.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="group flex cursor-pointer items-center justify-between">
+    <h2 class="text-4xl text-primary duration-300 group-hover:text-secondary">
+      projects
+    </h2>
+
+    <svg
+      viewBox="0 0 21 28"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-5 w-5 fill-primary duration-300 group-hover:fill-secondary"
+    >
+      <path
+        d="M0.666651 1.99999V26C0.667412 26.243 0.73443 26.4812 0.860487 26.6889C0.986546 26.8966 1.16687 27.066 1.38205 27.1789C1.59724 27.2918 1.83913 27.3438 2.08169 27.3295C2.32426 27.3151 2.55831 27.2348 2.75865 27.0973L20.092 15.0973C20.8106 14.6 20.8106 13.4027 20.092 12.904L2.75865 0.903992C2.55873 0.765092 2.32456 0.683637 2.08159 0.668479C1.83862 0.65332 1.59615 0.705037 1.38051 0.81801C1.16487 0.930984 0.984312 1.10089 0.858458 1.30928C0.732605 1.51766 0.666267 1.75655 0.666651 1.99999Z"
+      />
+    </svg>
+  </div>
+</template>
