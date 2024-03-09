74aa934ca69b34685de34b67c132b74ded729850
Miroslav Lincovsci <lincovsci02@gmail.com>
2024-03-09 11:20:09 +0300

    feat(components): add footer comonent

---
+++ src/components/Footer.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+function getCurrentYear(): number {
+  return new Date().getFullYear();
+}
+</script>
+<template>
+  <h5 class="text-primary-variant duration-300">
+    &copy; {{ getCurrentYear() }}. Miroslav Lincovsci. All rights reserved.
+  </h5>
+</template>
