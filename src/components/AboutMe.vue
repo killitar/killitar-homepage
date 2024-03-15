fcc730628d2f3b1151d605cee44e951a497875af
Miroslav Lincovsci <lincovsci02@gmail.com>
2024-03-15 17:46:14 +0300

    fix(components): fix duration on change color theme

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -2,7 +2,7 @@
   <div>
     <h2 class="animate-slideIn mb-10 text-4xl text-secondary duration-300">
       hello,<br />
-      i'm <span class="text-primary">miroslav lincovsci</span>.👋
+      i'm <span class="text-primary duration-300">miroslav lincovsci</span>.👋
     </h2>
     <p class="animate-fadeIn text-lg text-secondary duration-300">
       a front-end web developer, currently studying at the Russian University of
