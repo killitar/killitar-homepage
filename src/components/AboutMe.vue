49d62cb06f24e1d1f342fb923776fb581f4c8c7d
Miroslav Lincovschi <lincovsci02@gmail.com>
2024-12-20 17:32:45 +0300

    refactor: change about me description

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -4,14 +4,11 @@
       class="mb-10 animate-slideIn text-3xl text-secondary duration-300 lg:text-4xl"
     >
       hello,<br>
-      i'm <span class="text-primary duration-300">miroslav lincovsci</span>.👋
+      i'm <span class="text-primary duration-300">miroslav lincovschi</span>.👋
     </h2>
-    <p
-      class="hidden animate-fadeIn text-base text-secondary duration-300 lg:block lg:text-lg"
-    >
-      a front-end web developer, currently studying at the Russian University of
-      Cooperation. i love how the web can connect people, it inspires me to get
-      better and keep learning.
+    <p class="hidden animate-fadeIn text-base text-secondary duration-300 lg:block lg:text-lg">
+      i'm a front-end developer driven by creativity and the challenge of crafting user-friendly, beautiful web
+      experiences. every project is an opportunity to innovate and make the digital world more accessible and engaging.
     </p>
   </div>
 </template>
