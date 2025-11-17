44a01337c4822a2b99e5c65c0e9dfe37abb340b1
Miroslav Lincovschi <lincovschi@proton.me>
2025-11-17 15:57:02 +0300

    feat(AboutMe): remove emoji from hello message

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -4,7 +4,7 @@
       class="mb-10 animate-slideIn text-3xl text-secondary duration-300 lg:text-4xl"
     >
       hello,<br>
-      i'm <span class="text-primary duration-300">miroslav lincovschi</span>.👋
+      i'm <span class="text-primary duration-300">miroslav lincovschi</span>.
     </h2>
     <p class="hidden animate-fadeIn text-base text-secondary duration-300 lg:block lg:text-lg">
       i'm a front-end developer driven by creativity and the challenge of crafting user-friendly, beautiful web
